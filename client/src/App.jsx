a686acb70876c2156c72f78e9726c4cbdc5d2ae0
elyuli.dev <elyuli.dev@gmail.com>
2023-05-11 17:08:11 +0200

    Create logic <App/>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import styles from "./App.module.css";
+import Navbar from "./components/navbar/Navbar";
+import Card from "./components/card/Card";
+import posts from "./assets/data";
+
 function App() {
+	const [username, setUsername] = useState("");
+	const [user, setUser] = useState("");
+
 	return (
-		<>
-			<h1>Hello World!!!</h1>
-		</>
+		<div className={styles.container}>
+			{user ? (
+				<>
+					<Navbar />
+					{posts.map((post) => (
+						<Card key={post.id} post={post} />
+					))}
+
+					<span className={styles.username}>{username}</span>
+				</>
+			) : (
+				<div className={styles.login}>
+					<input
+						type="text"
+						placeholder="username"
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<button onClick={() => setUser(username)}>Login</button>
+				</div>
+			)}
+		</div>
 	);
 }
 
