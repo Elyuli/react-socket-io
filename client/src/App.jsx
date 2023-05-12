177f28031496fbf2a1ece28d922641e7a44ff67f
elyuli.dev <elyuli.dev@gmail.com>
2023-05-12 14:24:49 +0200

    import socket.io-client and <Navbar/> and <Card/>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 import Navbar from "./components/navbar/Navbar";
 import Card from "./components/card/Card";
 import posts from "./assets/data";
+import { io } from "socket.io-client";
 
 function App() {
 	const [username, setUsername] = useState("");
 	const [user, setUser] = useState("");
+	const [socket, setSocket] = useState(null);
+
+	useEffect(() => {
+		const newSocket = io("http://localhost:3500", {
+			transports: ["websocket"],
+		});
+		setSocket(newSocket);
+		return () => newSocket.close();
+	}, []);
+
+	useEffect(() => {
+		socket?.emit("newUser", user);
+		return () => socket?.removeListener("newUser");
+	}, [socket, user]);
 
 	return (
 		<div className={styles.container}>
 			{user ? (
 				<>
-					<Navbar />
+					<Navbar socket={socket} />
 					{posts.map((post) => (
-						<Card key={post.id} post={post} />
+						<Card key={post.id} post={post} socket={socket} user={user} />
 					))}
 
 					<span className={styles.username}>{username}</span>
